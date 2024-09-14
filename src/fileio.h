2ae7c43b48d835f7f90721e9775a739dd4d3b750
Adam Lynch <adamclynch03@gmail.com>
2024-09-14 15:23:52 -0400

    style: lines

--- src/fileio.h
+++ src/fileio.h
@@ -7,7 +7,6 @@
 #define FILEIO_WRITE_ASCII "w"
 #define FILEIO_READ_ASCII "r"
 
-void fileio_write(const char* path, void* bytes, int size, const char* mode);
+void fileio_write(const char *path, void *bytes, int size, const char *mode);
 
-void fileio_read(const char* path, void** bytes, int* size, const char* mode);
-
+void fileio_read(const char *path, void **bytes, int *size, const char *mode);
