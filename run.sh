d0daac0bf8d03f806f6fd36cba290b4cb75748bb
Adam Lynch <adamclynch03@gmail.com>
2024-09-24 18:45:58 -0400

    build: rename executable

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-release/out/build/opt_file_comp "/Users/adamlynch/Documents/HobbyWork/csv-comp/files/aapl=2024-09-03.csvc" "/Users/adamlynch/Documents/HobbyWork/csv-comp/files/aapl=2024-09-03.csv"
+release/out/build/csv_comp "/Users/adamlynch/Documents/HobbyWork/csv-comp/files/aapl=2024-09-03.csvc" "/Users/adamlynch/Documents/HobbyWork/csv-comp/files/aapl=2024-09-03.csv"
